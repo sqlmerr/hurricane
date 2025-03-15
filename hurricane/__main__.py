b0f4e42c39487af7e70399ecc5a62bca9ff03a13
sqlmerr <sqlmerr@ya.ru>
2025-03-15 18:48:35 +0300

    feat(restart cmd): test.restart command

--- hurricane/__main__.py
+++ hurricane/__main__.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import time
 from pathlib import Path
 
 from pyrogram import Client, idle
--- hurricane/modules/test.py
+++ hurricane/modules/test.py
@@ -1,7 +1,8 @@
+import atexit
+import os
+import sys
 import time
 
-from aiogram.types import CallbackQuery, CopyTextButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from pyrogram.types import Message
 
 import hurricane
@@ -10,7 +11,6 @@ from hurricane.addons.inline.components import Text, UrlButton, ClickableButton,
 from hurricane.addons.inline.form import FormAddon
 from hurricane.addons.translate import TranslateAddon
 from hurricane.inline.custom import HurricaneCallbackQuery
-from hurricane.types import ReplyMarkup
 
 async def on_click(call: HurricaneCallbackQuery):
     await call.answer("arbuz")
@@ -30,8 +30,8 @@ class TestMod(hurricane.Module):
     def __init__(self):
         self.t = TranslateAddon(
             self,
-            ru={"ping_txt": "🏓 <b>Понг! {}</b>"},
-            en={"ping_txt": "🏓 <b>Pong! {}</b>"},
+            ru={"ping_txt": "🏓 <b>Понг! {}</b>", "restart_txt": "🔄 <b>Hurricane перезагружается...</b>", "restarted": "✅ <b>Юзербот успешно перезагрузился за {} секунд</b>"},
+            en={"ping_txt": "🏓 <b>Pong! {}</b>", "restart_txt": "🔄 <b>Hurricane is restarting...</b>", "restarted": "✅ <b>Userbot successfully restarted in {} seconds</b>"},
         )
 
         self.c = CommandAddon(self)
@@ -39,21 +39,24 @@ class TestMod(hurricane.Module):
             simple_command("ping", self.ping_command, is_global=True),
             simple_command("crash", self.crash_cmd),
             simple_command("inline", self.inline_menu_cmd),
+            simple_command("restart", self.restart_cmd, is_global=True),
         )
 
         self.form = FormAddon(self)
 
+    async def on_load(self):
+        if msg_id := self.db.get("core.start", "message_id"):
+            chat_id = self.db.get("core.start", "chat_id")
+            start = self.db.get("core.start", "restarted_at")
+            end = time.perf_counter()
+            await self.client.edit_message_text(chat_id=chat_id, message_id=msg_id, text=self.t.restarted(round(end - start, 2)))
+
+            self.db.set("core.start", "message_id", None)
+            self.db.set("core.start", "restarted_at", None)
+
     async def inline_menu_cmd(self, message: Message, context: CommandContext):
         b = Builder(Menu)
         await b.build(message, self.form)
-
-    async def inline_callback(self, call: HurricaneCallbackQuery):
-        await call.answer("Hello", show_alert=True)
-
-        b = InlineKeyboardBuilder()
-        b.button(text="arbuz", copy_text=CopyTextButton(text="arbuzz"))
-
-        await call.edit("Hiii", b.as_markup())
 
     async def crash_cmd(self, _, __) -> None:
         result = 1 / 0
@@ -63,3 +66,15 @@ class TestMod(hurricane.Module):
         await message.edit(self.t.ping_txt(""))
         end = time.perf_counter()
         await message.edit(self.t.ping_txt(round(end - start / 10**6, 3)))
+
+    async def restart_cmd(self, message: Message, context: CommandContext) -> None:
+        def restart():
+            os.execl(sys.executable, sys.executable, "-m", "hurricane")
+
+        await message.edit(self.t.restart_txt())
+        self.db.set("core.start", "message_id", message.id)
+        self.db.set("core.start", "chat_id", message.chat.id)
+        self.db.set("core.start", "restarted_at", time.perf_counter())
+        atexit.register(restart)
+
+        return sys.exit(0)
